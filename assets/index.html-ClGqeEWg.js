89535790b45a2878f607ed20f0855ef93a38699e
mfei8 <69749498+mfei8@users.noreply.github.com>
2024-04-16 01:13:48 +0000

    Deploying to gh-pages from @ mfei8/blog@e37899d5b1b40e2b9861129690b2da5037fff2fb 🚀

---
+++ assets/index.html-ClGqeEWg.js
@@ -0,0 +1,78 @@
+import{_ as n}from"./plugin-vue_export-helper-DlAUqK2U.js";import{o as t,c as o,a as e}from"./app-CMbKp2X3.js";const p={};function s(r,a){return t(),o("div",null,[e(` ## 🛠 安装
+
+在当前目录下的 \`[dir]\` 文件夹内创建 vuepress-theme-hope 项目:
+
+::: code-tabs#shell
+
+@tab pnpm
+
+\`\`\`bash
+pnpm create vuepress-theme-hope [dir]
+\`\`\`
+
+@tab npm
+
+\`\`\`bash
+npm init vuepress-theme-hope@latest [dir]
+\`\`\`
+
+:::
+
+要将 vuepress-theme-hope 作为文档构建器添加到现有项目中，请在项目根目录中运行以下命令:
+
+::: code-tabs#shell
+
+@tab pnpm
+
+\`\`\`bash
+pnpm create vuepress-theme-hope add [dir]
+\`\`\`
+
+@tab npm
+
+\`\`\`bash
+npm init vuepress-theme-hope@latest add [dir]
+\`\`\`
+
+:::
+
+## 🚀 使用
+
+::: code-tabs#language
+
+@tab TS
+
+\`\`\`ts title=".vuepress/config.ts"
+import { defineUserConfig } from "vuepress";
+import { hopeTheme } from "vuepress-theme-hope";
+
+export default defineUserConfig({
+  // 站点选项
+  // ...
+
+  theme: hopeTheme({
+    // 主题选项
+    // ...
+  }),
+});
+\`\`\`
+
+@tab JS
+
+\`\`\`js title=".vuepress/config.js"
+import { hopeTheme } from "vuepress-theme-hope";
+
+export default {
+  // 站点选项
+  // ...
+
+  theme: hopeTheme({
+    // 主题选项
+    // ...
+  }),
+};
+\`\`\`
+
+::: `),e(` ## 官方 QQ 群
+
+- [点击加入](https://jq.qq.com/?_wv=1027&k=rATJyxGK) (群号: 1003437555) `),e(" markdownlint-disable ")])}const h=n(p,[["render",s],["__file","index.html.vue"]]),d=JSON.parse('{"path":"/","title":"行舟的技术成长之路","lang":"zh-CN","frontmatter":{"home":true,"title":"行舟的技术成长之路","icon":"home","heroImage":"/assets/icon/devise/纯黑.png","heroImageDark":"/assets/icon/devise/纯白.png","heroText":"行舟的技术成长之路","tagline":null,"actions":null,"head":[["meta",{"property":"og:url","content":"https://mfei.fun/blog/"}],["meta",{"property":"og:site_name","content":"行舟的技术成长之路"}],["meta",{"property":"og:title","content":"行舟的技术成长之路"}],["meta",{"property":"og:type","content":"website"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2024-04-16T01:13:03.000Z"}],["meta",{"property":"article:author","content":"Mr.MFei"}],["meta",{"property":"article:modified_time","content":"2024-04-16T01:13:03.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"WebPage\\",\\"name\\":\\"行舟的技术成长之路\\"}"]]},"headers":[],"git":{"createdTime":1710693409000,"updatedTime":1713229983000,"contributors":[{"name":"mfei8","email":"2066035486@qq.com","commits":5}]},"readingTime":{"minutes":2.68,"words":803},"filePathRelative":"README.md","localizedDate":"2024年3月17日","excerpt":"<!-- ## 🛠 安装\\n\\n在当前目录下的 `[dir]` 文件夹内创建 vuepress-theme-hope 项目:\\n\\n::: code-tabs#shell\\n\\n@tab pnpm\\n\\n```bash\\npnpm create vuepress-theme-hope [dir]\\n```\\n\\n@tab npm\\n\\n```bash\\nnpm init vuepress-theme-hope@latest [dir]\\n```\\n\\n:::\\n\\n要将 vuepress-theme-hope 作为文档构建器添加到现有项目中，请在项目根目录中运行以下命令:\\n\\n::: code-tabs#shell\\n\\n@tab pnpm\\n\\n```bash\\npnpm create vuepress-theme-hope add [dir]\\n```\\n\\n@tab npm\\n\\n```bash\\nnpm init vuepress-theme-hope@latest add [dir]\\n```\\n\\n:::\\n\\n## 🚀 使用\\n\\n::: code-tabs#language\\n\\n@tab TS\\n\\n```ts title=\\".vuepress/config.ts\\"\\nimport { defineUserConfig } from \\"vuepress\\";\\nimport { hopeTheme } from \\"vuepress-theme-hope\\";\\n\\nexport default defineUserConfig({\\n  // 站点选项\\n  // ...\\n\\n  theme: hopeTheme({\\n    // 主题选项\\n    // ...\\n  }),\\n});\\n```\\n\\n@tab JS\\n\\n```js title=\\".vuepress/config.js\\"\\nimport { hopeTheme } from \\"vuepress-theme-hope\\";\\n\\nexport default {\\n  // 站点选项\\n  // ...\\n\\n  theme: hopeTheme({\\n    // 主题选项\\n    // ...\\n  }),\\n};\\n```\\n\\n::: -->"}');export{h as comp,d as data};
